dad4ec67fd342a8536ab129a51f95e44bba52264
ani1010g <anisioaleixo@yahoo.com.br>
2018-11-13 20:46:26 -0200

    Criando campos para cadastro de clientes

--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
+
+
 import { ClienteCadastroComponent } from './cliente-cadastro/cliente-cadastro.component';
 
 @NgModule({
   declarations: [ClienteCadastroComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    InputTextModule,
+    InputMaskModule,
+  ],
   exports: [ClienteCadastroComponent]
 })
 export class ClienteModule { }
